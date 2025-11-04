305d64d2a957060f2c5ffb6ec0499cfaa599ffca
Niki9526 <n01591561@humber.ca>
2025-11-04 15:17:24 -0500

    added code importing data, / endpoint, fixed spaceing

--- Assignments/2/assignment2/server.js
+++ Assignments/2/assignment2/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+
 app.use(express.static("public"));
 
+const {DESTINATION, locations} = require("./modules/data.js")
 
+// needed for EJS
+app.set("view engine", "ejs"); 
 
-
-
-
-
-
+app.get("/",(req,res)=>{
+    return res.render("index.ejs",{d: DESTINATION, l:locations});
+})
 
 const startServer = () => {
     console.log("STARTING Express web server")     
-    console.log(`server listening on: http://localhost:${HTTP_PORT}`) 
+    console.log(`server listening on: http://localhost:${port}`) 
 }
 app.listen(port, startServer)
