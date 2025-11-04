eda01ac5a38130ed5f0863cdf34eb1419d124f58
Niki9526 <n01591561@humber.ca>
2025-11-04 16:23:33 -0500

    added locations data

---
+++ Assignments/2/assignment2/modules/data.js
@@ -0,0 +1,75 @@
+const DESTINATION = "Toronto";
+
+locations = [
+  {
+    id: 1,
+    name: "CN Tower",
+    category: "Landmark",
+    address: "290 Bremner Blvd, Toronto, ON M5V 3L9",
+    comments: "Really fun, resturant up there was great, had a great time! The glass floor was great",
+    image:
+      "https://media.gettyimages.com/id/458613409/photo/cn-tower-toronto-ontario-canada.jpg?s=612x612&w=gi&k=20&c=NhXUowD2b4h3H2C0Yi_QyFX1aO9TGvFcHbKYU_2tanQ=",
+  },
+  {
+    id: 2,
+    name: "Royal Ontario Museum (ROM)",
+    category: "Museum",
+    address: "100 Queen's Park, Toronto, ON M5S 2C6",
+    comments: "Crystal facade is wild; loved the dinosaurs wing.",
+    image: "https://source.unsplash.com/1600x900/?museum,toronto",
+  },
+  {
+    id:3,
+    name: "Kensington Market",
+    category: "Neighorhood",
+    address: "Kensington Ave, Toronto, ON",
+    comments: "Wandered through vintage shops, tryed local food from vendors. So colorful and artsy.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/e/ef/Kensington_Market_Toronto_August_2017_01.jpg",
+
+  },
+  {
+    id: 3,
+    name: "Distillary District",
+    category: "Neighorhood",
+    address: "55 Mill Street, Toronto, ON",
+    comments: "Lovely cobblestones pathways, art galleries, stores, and resturants. Enjoyed sipping seasonal hot chocolate.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Gooderham.jpg/1920px-Gooderham.jpg",
+
+  },
+  {
+    id: 4,
+    name: "St. Lawrence Market",
+    category: "Market",
+    address: "93 Front Sreet East, Toronto, ON",
+    comments: "Lovely place enjoyed it, lots of options there, nice food.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/St_Lawerence_South_Market_Exterior_202112.jpg/1920px-St_Lawerence_South_Market_Exterior_202112.jpg",
+  },
+  {
+    id:5,
+    name: "High Park",
+    category: "Park",
+    address: "1873 Bloor Street West, Toronto, ON",
+    comments: "Huge park in the downtown core, peacefull trails, spotted cherry blossoms!",
+    image: "https://lh3.googleusercontent.com/gps-cs-s/AG0ilSzed0s2xrwUH25Kzgqa05T-3t2Wq2lLC9fIJkYHVqtTZZsbFAQ3nQSjNXjk7cKh0c_pqSHdOVyk6UOXD_6rEyn_xzU1LLXGH3zyHGjmeqLmYzel9jYdSwj-YDnolwri9HoHH_rU=s1360-w1360-h1020-rw",
+
+  },
+  {
+    id: 6,
+    name: "Nathan Phillips Square",
+    category: "Landmark",
+    address: "100 Queen Street West, Toronto, ON",
+    comments: "Skated here at the rink, took picture by the sign, magic!",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Nathan_Philips_Square%2C_2022.jpg/1920px-Nathan_Philips_Square%2C_2022.jpg"
+
+  },
+  {
+    id: 7,
+    name: "Case Loma",
+    category: "Landmark",
+    address: "1 Austin Terrace, Toronto, ON",
+    comments: "Felt like royalty exploring secret passageways in a castle above the city.",
+    image: "https://lh3.googleusercontent.com/gps-cs-s/AG0ilSy3Teh0d3r7-GnX48ku13NcPd2yXtl04rZjtDulysekWWVWSzcMZuibGLgF1-fYJnu0SOTsqnraOD-JIOKzHE9zUP0vMVlQCNxMs7D54sTObUXXqBPu41wXUVTRO13UHMRETCzAmQ=s1360-w1360-h1020-rw",
+  }
+];
+
+module.exports = {locations,DESTINATION};
