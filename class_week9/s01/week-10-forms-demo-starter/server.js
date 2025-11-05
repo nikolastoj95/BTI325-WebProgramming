adf55a83339921feadbc8a540b46c3940c96bd41
Niki9526 <n01591561@humber.ca>
2025-11-05 09:14:52 -0500

    inital commit

---
+++ class_week9/s01/week-10-forms-demo-starter/server.js
@@ -0,0 +1,36 @@
+const HTTP_PORT = process.env.PORT || 8080;
+
+const express = require("express");
+const app = express();
+app.use(express.static("public"));  // css files
+app.set("view engine", "ejs");      //ejs
+
+// forms: process data received in a req.body
+// need this for req.body
+app.use(express.urlencoded({ extended: true }));    
+
+app.get("/", (req, res) => {    
+    return res.render("home.ejs")
+})
+
+// TODO: Make a POST endpoint
+
+app.post("/test",(req,res)=>{
+    console.log("Debug: Activing /test end point")
+    //get payload data
+    console.log("DEBUG. payload DATA from the form")
+    console.log(req.body)
+    return res.send("Success! Done")
+})
+
+
+
+// 5 use the req.body to access 
+// req.body gets you everything in payload 
+app.get("/test",(req,res)=> {
+    console.log("Data from form:")
+    console.log(req.body)
+    return res.send("done!")
+});
+
+app.listen(HTTP_PORT, () => { console.log(`server listening on: http://localhost:${HTTP_PORT}`) });
