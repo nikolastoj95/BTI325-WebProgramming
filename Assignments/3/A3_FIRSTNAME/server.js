f1836ecf72ba63956658c320e7a3a1f541b1a490
Niki9526 <n01591561@humber.ca>
2025-11-29 17:53:36 -0500

    added model, schemas for cars, users, anded logic on endpoints

--- Assignments/3/A3_FIRSTNAME/server.js
+++ Assignments/3/A3_FIRSTNAME/server.js
@@ -23,33 +23,196 @@ app.use(express.static("public"));  // css files
 
 // TODO: Put your model and schemas here
 
+const carSchema = new mongoose.Schema({
+    model: String,
+    imageUrl: String,
+    returnDate: { type: String, default: ''} ,
+
+    // 1 to 1 relationship 
+    rentedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'users', default: null}
+
+})
+const Car = new mongoose.model("cars",carSchema)
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+const User = new mongoose.model("users",userSchema)
 
 // TODO: Modify your endpoint logic
 app.get("/", async (req, res) => {  
     return res.render("login.ejs")
 })
 app.post("/login", async (req, res)=>{
+     console.log(req.body)
+    // const eMail = req.body.email
+    // const password = req.body.password
+
+    //console.log(eMail + ' and ' + password)
+    //1. search DB for entered info in users collection Find user by email
+    const user = await User.findOne({
+        email: req.body.email
+    })
+    
+    // 2.  If email not found -> create User
+    if (!user) {
+        await User.insertOne({
+            email: req.body.email,
+            password: req.body.password
+        })  
+    }
+    //3. If email found but password wrong -> error
+    if (user.password !== req.body.password){
+        //alert('ERROR! Wrong username or password!')
+        return res.redirect("/")
+    }
+
+    //4. Email + password match -> success
+
+    console.log(req.sessionID)
+    console.log(req.session)
+    
+    req.session.userInfo = {
+        email: req.body.email
+    }
+     console.log(req.session.userInfo.email)
+     
+
+
+
+    // const result = await User.findOne({
+    //     email: req.body.email,
+    //     password: req.body.password
+    // })
+    //console.log(result)
+    // find it now
+    // if (result === null) {
+    //     // no user found create them
+    //     await User.insertOne({
+    //         email: req.body.email,
+    //         password: req.body.password
+    //     })
+    //     console.log('created user')
+
+    // } else {
+    //     // user found, matching, log them in 
+    //     console.log(result)
+    //     return res.redirect("/cars")
+    // }
     return res.redirect("/cars")
 })
 app.get("/logout", async (req,res) => {
+    req.session.destroy()
     return res.redirect("/")
 })
 app.get("/cars", async (req, res) => {  
-    return res.render("cars.ejs")
-})
+    let loggedIn = false
+    
+
+    if (req.session.userInfo === undefined ) {
+        loggedIn = false
+        return res.redirect("/")
+    } else {
+        loggedIn = true
+        const session = req.session.userInfo.email
+        console.log(session)
+        const cars =  await Car.find({})
+        return res.render("cars.ejs", {car:cars, loggedIn, session })
+    }
+});
 app.get("/book", async (req,res)=>{
-    return res.render("bookingForm.ejs")
+
+    let loggedIn = false
+
+    if(req.session.userInfo === undefined) {
+        loggedIn = false
+        return res.redirect("/")
+    } else {
+        loggedIn= true
+        const session = req.session.userInfo.email
+        return res.render("bookingForm.ejs")
+
+    }
+
+
+
+    
 })
 app.post("/book", async (req,res)=>{
     // get booking form data 
+    console.log(req.body)
+    console.log(req.body.date)
+    const currUser = await User.findOne({
+        email: req.session.userInfo.email
+    })
+    console.log(currUser.email)
+
+
     return res.redirect("/cars")
 })
+
+const prepopulateDB =  async () => {
+    const count = await Car.countDocuments()
+
+    if ( count === 0 ) {
+
+        //const userNikola= await User.create({email: 'nikola@gmail.com', password:'1234'})
+        //const userCeleste = await User.create({email: 'celeste@gmail.com', password: 'CelesteIsBaby2'})
+
+        await Car.insertMany([
+            {
+              model: "Honda Civic", 
+              imageUrl: "https://di-uploads-pod11.dealerinspire.com/appletreehonda/uploads/2018/07/2018-Honda-Civic-Header-Hero.png",
+              returnDate: "",
+              rentedBy: null
+            },
+             {
+              model: "Telsa Model 3", 
+              imageUrl: "https://www.tesla.com/ownersmanual/images/GUID-B5641257-9E85-404B-9667-4DA5FDF6D2E7-online-en-US.png",
+              returnDate: "",
+              rentedBy: null
+
+            },
+             {
+              model: "Mercedes C400", 
+              imageUrl: "https://i.ytimg.com/vi/mactcc43Jrs/maxresdefault.jpg",
+              returnDate: "",
+              rentedBy: null
+            },
+             {
+              model: "Hyundai Elantra", 
+              imageUrl: "https://content-images.carmax.com/qeontfmijmzv/2eNGdQLX4jmD3xNHx3IBnv/60036560e1da4d21c3b5eb1b57dd7daa/elantra-gen-c.png?w=2100&fm=webp",
+              returnDate: "",
+              rentedBy: null
+            },
+             {
+              model: "Ford Mustang", 
+              imageUrl: "https://imgd.aeplcdn.com/664x374/cw/ec/23766/Ford-Mustang-Exterior-126883.jpg?wm=0&q=80",
+              returnDate: "",
+              rentedBy: null
+            }
+            
+        ])
+
+        
+            
+        
+
+        console.log("DEBUG: populated cars collection ");
+    } else {
+        console.log("DEBUG: Cars collection contains documents, cars skipping ");
+    }
+
+}
 
 
 async function startServer() {
     try {    
         // TODO: Update this
         await mongoose.connect(process.env.MONGO_URI)
+
+        prepopulateDB()
 
         console.log("SUCCESS connecting to MONGO database")
         console.log("STARTING Express web server")        
