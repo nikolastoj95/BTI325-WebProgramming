c471df726561afdd99451e77ff604b0b914d1825
Niki9526 <n01591561@humber.ca>
2025-12-05 15:14:37 -0500

    inital commit

---
+++ class_Week13/s01/example01/server.js
@@ -0,0 +1,26 @@
+const HTTP_PORT = process.env.PORT || 8080;
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {        
+    let html = `      
+        <h1>Welcome to my Webpage!</h1>
+        <p>Put some stuff here!</p>
+    `
+    return res.send(html)
+})
+
+const startServer = async () => {    
+    try {    
+        console.log("STARTING Express web server")        
+        app.listen(HTTP_PORT, () => {     
+            console.log(`server listening on: http://localhost:${HTTP_PORT}`) 
+        })    
+    }
+    catch (err) {        
+        console.log("Please resolve these errors and try again.")
+        console.log(err)
+    }
+}
+startServer()
