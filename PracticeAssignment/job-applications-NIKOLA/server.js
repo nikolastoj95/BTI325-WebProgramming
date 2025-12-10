1f8b24be3d56b0596b795e25035bc6876b2dc54f
Niki9526 <n01591561@humber.ca>
2025-12-09 23:11:03 -0500

    added home endpoint logic for curr user logged in, added apply for job, added show apps, added login fixed person sechma is admin default false

--- PracticeAssignment/job-applications-NIKOLA/server.js
+++ PracticeAssignment/job-applications-NIKOLA/server.js
@@ -21,13 +21,15 @@ app.use(session({
 const mongoose = require('mongoose')
 
 // model and sechma
+//person
 const personSchema = new mongoose.Schema ({
     name: String,
     password: String,
-    isAdmin: Boolean
+    isAdmin: {type: Boolean, default: false}
 })
 const Person = new mongoose.model("persons",personSchema)
 
+//job
 const jobSchema = new mongoose.Schema({
     title: String,
     description: String,
@@ -35,38 +37,157 @@ const jobSchema = new mongoose.Schema({
 })
 const Job = new mongoose.model("jobs", jobSchema)
 
+//application
+// an app has a job
+// an app had a person
 const appSchema = new mongoose.Schema({
     job: {type: mongoose.Schema.Types.ObjectId, ref: "jobs"},
     user: {type: mongoose.Schema.Types.ObjectId, ref:"persons"}
 })
-const Application =new mongoose.model("apps",appSchema  )
+const Application =new mongoose.model("apps",appSchema)
+
+  
+
+app.get("/", async (req, res) => {   
+      
+    // view all jobs
+    let loggedIn = false
+    if (req.session.userInfo === undefined) {
+        loggedIn = false
+        // database
+        const showJob = await Job.find({})
+        console.log(showJob)
+        return res.render("home.ejs", {job: showJob , loggedIn})
+        // go to ejs home to show the jobs
+   
+   }
+        loggedIn = true
+        const nameSession = req.session.userInfo.name
+        const currUser = req.session.userInfo 
+       
+        const showJob = await Job.find({})
+        return res.render("home.ejs", {loggedIn, name:nameSession, currUser, job: showJob })
+  
 
 
-app.get("/", async (req, res) => {        
-    return res.render("home.ejs")
+     
 })
+// GET
+//apply for a job
+app.get("/apply/job/:jobID", async (req,res)=>{
+    const currUser = req.session.userInfo;
+    console.log(currUser)
+
+    //TODO Only logged in users can apply
+    if (req.session.userInfo === undefined) {
+        console.log("Can not apply, you are not logged")
+        return res.redirect("/")
+    }
+    //TODO Ensure that admins can not apply
+    if (currUser.admin === true) {
+        //return res.send("You can not apply cause you are admin")
+        console.log("You can not apply cause you are admin")
+        return res.redirect("/")
+    }
+
+
+ // bring in param job id
+    const jobID = req.params.jobID;
+    console.log(jobID)
+
+    
+    await Application.create({
+        job: jobID,
+        user: currUser._id
+    })
+    return res.redirect("/")
+});
+
+//GET
+//show application page
+app.get("/applications", async (req, res) => {
+    if (req.session.userInfo === undefined) {
+        return res.redirect("/")
+    }
+
+   const currUser = req.session.userInfo;
+   console.log(currUser)
+
+   let applications = []
+
+    if (currUser.admin === true ) {
+        //admin users can see all apps
+        applications = await Application.find().populate("job").populate("user")
+   } else {
+        // showing only logged in user
+        // logged in user can see only there apps
+        // find filter is the curr user id finding the user field in application if that id matchs
+        applications = await Application.find({user: currUser._id}).populate("job").populate("user")
+   }
+
+    //const showapp = await Application.find().populate("job").populate("user")
+    console.log(applications)
+    return res.render("applications.ejs",{app:applications});
+});
+
 
 app.get("/login", (req, res) => {
+    // to show form
     res.render("login.ejs");
 });
 app.post("/login", async (req, res) => {
     // endpoint to login
-    return res.send("login post endpoint")
+    // to retirve form fields
+     console.log(req.body)
+     console.log(typeof req.body.password)
+
+     //1. find a user, find name n DB
+      let user = await Person.findOne({
+        name: req.body.name
+      })
+      console.log(user)
+
+      // 2. if name not in db create the user
+      if (!user) {
+        user = await Person.create({
+            name: req.body.name,
+            password: req.body.password
+        })
+        console.log("user created, procede to log in")
+      } else   {
+            //3. email there but  password wrong 
+            if (user.password !== req.body.password) {
+                console.log("error")
+                return res.redirect("/login")
+            } 
+      } 
+        // matches  all name + password
+        //update session with this user 
+        console.log(req.sessionID)
+        console.log(req.session)
+
+       // create a user session
+        req.session.userInfo = {
+            _id: user._id.toString(),
+            name: user.name,
+            admin: user.isAdmin
+        }
+        console.log(req.session.userInfo)
+
+    return res.redirect("/")
 })
 
 app.get("/logout", (req, res) => {
-    return res.send("logout")
-});
-
-app.get("/applications", async (req, res) => {
-    return res.render("applications.ejs");
+    req.session.destroy()
+    console.log("logged out")
+    return res.redirect("/")
 });
 
 const populateDatabase = async () => {
     // do osmething ehre
-    const countJ = await Job.countDocuments()
+    //const countJ = await Job.countDocuments()
     const countPer = await Person.countDocuments()
-    if (countJ &&  countPer === 0) {
+    if (countPer === 0) {
         await Job.insertMany([
             {
               title: "Data Analysit",
@@ -79,10 +200,9 @@ const populateDatabase = async () => {
               {
               title: "Frontend Developer",
               description: "Responsible for creating stunning user interfaces for the web.", 
-              isOpen: false}
+              isOpen: false},
               
         ])
-
         await Person.insertMany([
                 {
                     name: "adamf@eNet.com",
@@ -98,22 +218,13 @@ const populateDatabase = async () => {
                     name: "Celeste Prov",
                     password: "CelesteIsABaby5",
                     isAdmin: false
-                },
-
+                }
         ])
-
         console.log("Person and Jobs collections populated!")
-
-
     }else {
         console.log("Collection already populated, skipping")
-
     }
-
 }
-
-
-
 async function startServer() {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
