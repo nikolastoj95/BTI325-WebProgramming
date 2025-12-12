9dd1d7320f6ca6f604a75cb1e03b739578b08678
Niki9526 <n01591561@humber.ca>
2025-12-12 10:06:04 -0500

    fixed spaces

--- Test-4/BOOKING_NIKOLA/server.js
+++ Test-4/BOOKING_NIKOLA/server.js
@@ -68,12 +68,9 @@ app.post("/book/:id", async (req,res)=>{
 })
 app.get("/remind/:id", async (req,res)=>{   
     const remindID = req.params.id
-    //console.log(remindID)
+    console.log(remindID)
     console.log(req.sessionID)
 
-
-    
-    
     return res.send(`Reminder feature activated! <a href="/">Home</a>`)
 })
 
